de22218cb5ffc50f5999d160181fa358b17c889e
fauzanhilmi <fauzan_hilmi@students.itb.ac.id>
2014-12-19 00:12:16 +0700

    first commit

---
+++ src/java/classes/User.java
@@ -0,0 +1,21 @@
+package classes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author USER
+ */
+public class User {
+    public String Nama;
+    public String email;
+    public String username;
+    public String pass;
+    public int tipe;
+    
+    public User(String n) {Nama = n;}
+}
+
---
+++ src/java/classes/tes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author USER
+ */
+public class tes {
+    public static void main(String args[]) {
+        tes t = new tes();
+        System.out.println(addUser("lkjlfdask","lkfdjldkfasj","kldfslkfdsaj","dakjflda","Admin"));
+        
+    }
+
+    private static boolean addUser(java.lang.String arg0, java.lang.String arg1, java.lang.String arg2, java.lang.String arg3, java.lang.String arg4) {
+        org.chamerling.heroku.service.HelloServiceImplService service = new org.chamerling.heroku.service.HelloServiceImplService();
+        org.chamerling.heroku.service.HelloService port = service.getHelloServiceImplPort();
+        return port.addUser(arg0, arg1, arg2, arg3, arg4);
+    }
+            
+}
